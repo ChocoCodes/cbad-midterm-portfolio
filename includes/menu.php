0bb62535699d1d84fa2bc056d355fbb86fbc3836
ChocoCodes <chocojhong999@gmail.com>
2025-08-24 23:21:03 +0800

    chore: menu nav links positioning

--- includes/menu.php
+++ includes/menu.php
@@ -5,26 +5,25 @@
 <div id="sidebar" class="col-sm-2 d-flex justify-content-between poppins-regular">
     <div id="sidebar-container" class="w-100 d-flex flex-column justify-content-between">
         <!-- Sidebar Header -->
-        <div id="sidebar-header" class="d-flex gap-2">
+        <div id="sidebar-header" class="d-flex gap-2 align-items-center">
             <img src="assets/img/svgs/logo.svg" alt="Logo">
             <p class="fs-4">John</br>Octavio</p>
         </div>
 
-        <!-- Sidebar Nav Items -->
-        <ul class="sidebar-nav d-flex flex-column gap-1 align-start">
-            <?php 
-                foreach($META as $Key => $Value) {
-                    echo "
-                        <li class='sidebar-nav-item fs-3'>
-                            <a href='{$Value['page']}'>{$Value['title']}</a>
-                        </li>
-                    ";
-                };
-            ?>
-        </ul>
-
         <!-- Sidebar Footer -->
         <div class="w-100 d-flex flex-column gap-3">
+            <!-- Sidebar Nav Items -->
+            <ul class="sidebar-nav d-flex flex-column gap-1 align-start">
+                <?php 
+                    foreach($META as $Key => $Value) {
+                        echo "
+                            <li class='sidebar-nav-item fs-3'>
+                                <a href='{$Value['page']}'>{$Value['title']}</a>
+                            </li>
+                        ";
+                    };
+                ?>
+            </ul>
             <div id="separator" class="w-100 rounded-5"></div>
             <div class="d-flex flex-column">
                 <span id="time"></span>
