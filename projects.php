df7b73e7b01e50533bf34fb9b9234fc6c161ec4c
ChocoCodes <chocojhong999@gmail.com>
2025-08-28 14:21:57 +0800

    fix: project card font size

--- projects.php
+++ projects.php
@@ -7,8 +7,8 @@
     include_once 'includes/menu.php';
 ?>
     <div class="col-12 col-md-10 main-container overflow-y-auto overflow-x-hidden d-flex justify-content-between align-items-center">
-        <div style="--glow-width: 23rem; --glow-height: 23rem; --glow-alpha: 0.85; top: -10%; right: -10%;" class="background-glow position-absolute rounded-circle"></div>
-        <div class="project-section main-content mx-auto d-flex flex-column position-relative">
+        <div style="--glow-width: 23rem; --glow-height: 23rem; --glow-alpha: 0.85; top: -5%; right: -10%;" class="background-glow position-absolute rounded-circle"></div>
+        <div class="project-section main-content mx-auto d-flex flex-column gap-2 position-relative">
             <!-- Page Header Section --> 
             <p class="poppins-medium text-heading fs-2 m-0"><?= $PageHeader ?></p>
             <!-- Project Cards Container -->
